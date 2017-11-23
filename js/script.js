c7967c0627a514a36cf7df7de00230cff8397e97
Pwang-je <rewind1224@gmail.com>
2017-11-24 03:35:32 +0900

    3-1
    fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-
-$(window).scroll(function(){
+/*$(window).scroll(function(){
   var sticky = $('#header-fixed'),
       scroll = $(window).scrollTop();
 
   if (scroll >= 100) sticky.addClass('h-fixed');
   else sticky.removeClass('h-fixed');
 });
+*/
+
+$(function(){
+    var menupos = $("#header-fixed").offset().top;
+    $(window).scroll(function(){
+       if($(window).scrollTop() >= menupos) {
+          $("#header-fixed").css("position","fixed");
+          $("#header-fixed").css("top","0");
+          $('#top_mar').show();
+          }
+    });
+ });
